475d7492dd95f70342cfcc67702e3a281fd48de1
Sasha Rochedix <sasharochedix@gmail.com>
2024-05-14 23:04:13 +0200

    First appereance of Main menu

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+
+// ANIMATION MENU //
+TweenMax.staggerFrom(".btn", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
+
+$(".btn").click(function(){
+  TweenMax.staggerTo(".btn", 0.5, {opacity:0, y:-100, ease:Back.easeIn}, 0.1);
+});
